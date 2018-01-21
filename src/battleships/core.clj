bbc45bb970566dca9cd41ea5054f90ca6b11d036
Satyarth Mishra Sharma <github@satyarth.me>
2018-01-21 14:09:18 +0000

    Initialize project

---
+++ src/battleships/core.clj
@@ -0,0 +1,7 @@
+(ns battleships.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/battleships/core_test.clj
@@ -0,0 +1,7 @@
+(ns battleships.core-test
+  (:require [clojure.test :refer :all]
+            [battleships.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
